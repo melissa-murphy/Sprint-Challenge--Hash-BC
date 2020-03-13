9554f8c390df67a4e6c7da920c096aefcac80a25
melissa-murphy <melissamurphy.ca@gmail.com>
2020-03-13 10:15:27 -0700

    working miner, but having some pipenv issues

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -1,13 +1,9 @@
 import hashlib
 import requests
-
 import sys
-
 from uuid import uuid4
-
 from timeit import default_timer as timer
-
-import random
+from random import randint
 
 
 def proof_of_work(last_proof):
@@ -23,10 +19,18 @@ def proof_of_work(last_proof):
     start = timer()
 
     print("Searching for next proof")
-    proof = 0
-    #  TODO: Your code here
+
+    proof = last_proof*randint(0, 100)
+
+    last_proof = f'{proof}'.encode()
+
+    last_hash = hashlib.sha256(last_proof).hexdigest()
+
+    while valid_proof(last_hash, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
+
     return proof
 
 
@@ -35,15 +39,17 @@ def valid_proof(last_hash, proof):
     Validates the Proof:  Multi-ouroborus:  Do the last six characters of
     the hash of the last proof match the first six characters of the hash
     of the new proof?
-
     IE:  last_hash: ...AE9123456, new hash 123456E88...
     """
+    guess = f'{proof}'.encode()
 
-    # TODO: Your code here!
-    pass
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    return last_hash[-6:] == guess_hash[:6]
 
 
 if __name__ == '__main__':
+
     # What node are we interacting with?
     if len(sys.argv) > 1:
         node = sys.argv[1]
@@ -61,18 +67,19 @@ if __name__ == '__main__':
     if id == 'NONAME\n':
         print("ERROR: You must change your name in `my_id.txt`!")
         exit()
+
     # Run forever until interrupted
     while True:
+
         # Get the last proof from the server
         r = requests.get(url=node + "/last_proof")
         data = r.json()
         new_proof = proof_of_work(data.get('proof'))
-
         post_data = {"proof": new_proof,
                      "id": id}
-
         r = requests.post(url=node + "/mine", json=post_data)
         data = r.json()
+
         if data.get('message') == 'New Block Forged':
             coins_mined += 1
             print("Total coins mined: " + str(coins_mined))
